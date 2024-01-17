5ff542953ae8b9e54674d953231144a2e97d9f57
Muhammad Safaat <safaat4@gmail.com>
2024-01-17 18:46:22 +0800

    first push

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function peringatan(){
+    let pertama = document.getElementById{"angka-pertama"}.value;
+    let kedua = document.getElementById{"angka-kedua"}.value;
+    
+    let hasil = parseInt{pertama} + parseInt{kedua}; 
+    console.log(hasil)
+    document.write('Hasilnya adalah ' + hasil)
+       
+}
